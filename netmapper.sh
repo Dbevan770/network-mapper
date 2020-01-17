a3b0fd3269e34d9b8170e012b55c464cc1a04601
Danny <Gingerbreadpk@gmail.com>
2020-01-17 15:38:51 -0600

    Network Mapper Tool
    Mapper written in Bash prompts user for network and range of IPs then pings each IP to check if it is valid. If the IP is valid runs a for loop on well known ports testing if they are open and returns that to the user.

---
+++ netmapper.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Ask for input from the user to specifiy what network to Enumerate
+
+read -p "Enter Network you wish to Enumerate XXX.XXX.XXX: " network
+
+# Ask user for range of IPs they wish to Enumerate in case they don't wish to check all 255 possibilities.
+
+read -p "Enter range of IPs you wish to Enumerate XX XX: " min max
+
+# Set 4th octet variable for loops
+
+Oct4=0
+
+# Define ports to check for
+
+ports=(20 21 22 25 43 53 69 79 80 88 110 111 115 118 137 138 139 156 220 443 513 514 543)
+
+while [ $Oct4 -ge $min ] && [ $Oct4 -le $max ]
+do
+	ipAddress="$network.$Oct4"
+	
+	ping -c 1 -W 1 $ipAddress > /dev/null
+	
+	if [ $? -eq 0 ]
+	then
+		echo "$ipAddress is a valid host; Ports host has open: "
+		for portNumber in ${ports[*]}
+		do
+			(echo "" > /dev/tcp/$ipAddress/$portNumber) 2> /dev/null
+			
+			if [ $? -eq 0 ]
+			then
+				echo "Port $portNumber is Open."
+			fi
+		done
+	fi
+	Oct4=`expr $Oct4 + 1`
+done
